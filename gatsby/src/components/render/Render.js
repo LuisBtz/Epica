13f3838ad47f059330064b2e0f3d8e26b16989a7
Luis B <luisbttf@gmail.com>
2023-10-16 21:48:44 -0600

    concepto

--- gatsby/src/components/render/Render.js
+++ gatsby/src/components/render/Render.js
@@ -31,6 +31,12 @@ const RenderContainer = styled.section`
 width: 100%;
 padding: 100px 50px;
 position: relative;
+z-index: 1;
+@media (max-width: 630px) {
+    padding-top: 0 !important;
+    display: flex;
+    flex-direction: column-reverse;
+}
     .image {
         width: 100%;
     }
@@ -39,15 +45,30 @@ position: relative;
         top: 150px;
         left: 50%;
         transform: translateX(-50%);
-        width: 1200px;
+        width: 100%;
         z-index: 1;
         color: var(--white);
         text-align: center;
          margin: 0 auto;
+         @media (max-width: 950px) {
+            width: 350px;
+            top: 120px;
+            p {
+                width: 100% !important;
+                padding-top: 20px;
+            }
+        }
+        @media (max-width: 630px) {
+            position: static;
+            color: var(--black);
+            transform: none;
+            margin-bottom: 50px;
+        }
         p {
             width: 700px;
             margin: 0 auto;
             line-height: 1.2;
+            
         }
     }
 `
--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import Layout from '../components/layout/layout'
 import { Seo } from '../components/layout/seo';
 import { graphql } from "gatsby";
 import { GatsbyImage, getImage } from "gatsby-plugin-image"
 import styled from "styled-components";
 import Render from "../components/render/Render";
+import BlockContent from '@sanity/block-content-to-react';
 
 
 export const data = graphql`
   query {
     sanityLandingPage {
+      _rawAddress
+        socialMedia {
+        title
+        link
+        icon {
+          textoAlternativo
+          asset {
+            url
+          }
+        }
+      }
       brownIcon {
         textoAlternativo
         asset {
@@ -46,24 +58,65 @@ export const data = graphql`
 
 
 
+
+
+
 const IndexPage = ({data}) => {
 
   const iconGetDataImage = getImage(data.sanityLandingPage.brownIcon && data.sanityLandingPage.brownIcon.asset)
   const iconGetDataImageAlt = data.sanityLandingPage.brownIcon && data.sanityLandingPage.brownIcon.textoAlternativo
 
 
+  const [nav, showNav] = useState(false);
+
+
   return (
     <Layout >
       <Main className="main">
-        <Aside>
-          <div className="image">
-              <GatsbyImage
-                  style={{ height: "100%", width: "100%" }}
-                  image={iconGetDataImage}
-                  alt={iconGetDataImageAlt}
-              />
+        <header className={nav ? 'menu active' : 'menu'} onClick={() => showNav(!nav)}>
+          <img src="/logo.svg" alt='Logo Epica'  className="logo" />
+          <div className="mid">
+            <a className="brochure" href='/brochure.pdf' download>descargar brochure</a>
+            <ul className="menu">
+              <li><a href='#concepto'>Concepto</a></li>
+              <li><a href='#ubicacion'>Ubicación</a></li>
+              <li><a href='#numeralia'>Numeralia</a></li>
+              <li><a href='#amenidades'>Amenidades</a></li>
+              <li><a href='#galeria'>Galería</a></li>
+              <li><a href="contacto">Contacto</a></li>
+            </ul>
+
           </div>
-        </Aside>
+          <div className="bottom">
+            <div className="address">
+              <BlockContent
+                blocks={data.sanityLandingPage._rawAddress}
+              />
+            </div>
+            <div className="social">
+              <ul>
+                {data.sanityLandingPage.socialMedia.map(({ title, link, icon }) => {
+                  return (
+                      <li><a target='_blank' rel="noreferrer" href={link}><img src={icon.asset.url} alt={title} /></a></li>
+                  )
+                })}
+              </ul>
+            </div>
+          </div>
+        </header>
+        <Button onClick={() => showNav(!nav)}>
+          <div className="sticky">
+            <div className="image">
+                <GatsbyImage
+                    style={{ height: "100%", width: "100%" }}
+                    image={iconGetDataImage}
+                    alt={iconGetDataImageAlt}
+                />
+            </div>
+            <p>MENU</p>
+          </div>
+          
+        </Button>
 
 
         {/* Render */}
@@ -81,14 +134,78 @@ const IndexPage = ({data}) => {
 const Main = styled.div`
   display: flex;
   position: relative;
+  height: 100%;
+  .active {
+    left: 0px !important;
+  }
+  header.menu {
+    position: fixed;
+    top: 0;
+    transition: all 350ms ease-in-out;
+    left: -350px;
+    width: 350px;
+    height: 100vh;
+    z-index: 50;
+    background-color: var(--white);
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    .logo {
+      margin-bottom: 50px;
+    }
+    .mid {
+      .brochure {
+        text-transform: uppercase;
+        text-align: center;
+        display: block;
+        width: 100%;
+        padding: 10px;
+        background-color: var(--black);
+        color: var(--white);
+      }
+      ul {
+        margin-top: 20px;
+        margin-bottom: 20px;
+        li {
+          border-top: solid 1px var(--black);
+          text-transform: uppercase;
+          padding-top: 10px;
+          padding-bottom: 10px;
+        }
+      }
+    }
+    .bottom {
+      .social {
+        ul {
+          display: flex;
+          margin-top: 30px;
+          li {
+            display: block;
+            width: 30px;
+            margin-right: 10px;
+          }
+        }
+      }
+    }
+  }
 `
 
-const Aside = styled.aside`
+const Button = styled.button`
+  display: flex;
+  flex-direction: column;
   width: 150px;
   padding: 50px;
   border-right: solid 1px var(--brown);
-  position: sticky;
-  top: 0;
+  position: relative;
+  .sticky {
+    position: sticky;
+    top: 50px;
+    width: 100%;
+    p {
+      margin-top: 75vh;
+    }
+  }
 `
 
 
